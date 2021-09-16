512c256b07e07413c62f490487a96a46fdabd266
dhaniemubarak <dhaniemubarak@gmail.com>
2021-09-17 01:01:27 +0700

    HOL-21 Init project

---
+++ src/main/java/id/holigo/services/holigoapigatewayserver/HoligoApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigoapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoligoApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoligoApiGatewayServerApplication.class, args);
+	}
+
+}
