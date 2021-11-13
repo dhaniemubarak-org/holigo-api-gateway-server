a2dbca944a35405d9630315c7cc2ba511a8e3e78
dhaniemubarak <dhaniemubarak@gmail.com>
2021-11-13 08:24:47 +0700

    HOL-69 Modified Raouter validator

--- src/main/java/id/holigo/services/holigoapigatewayserver/web/validators/RouterValidator.java
+++ src/main/java/id/holigo/services/holigoapigatewayserver/web/validators/RouterValidator.java
@@ -1,4 +1,4 @@
-package id.holigo.services.holigoapigatewayserver.validators;
+package id.holigo.services.holigoapigatewayserver.web.validators;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RouterValidator {
-        public static final List<String> openApiEndpoints = List.of("/api/v1/user/6285718187373", "/api/v1/otp",
-                        "/api/v1/register", "/api/v1/authenticate", "/h2-console");
+        public static final List<String> openApiEndpoints = List.of("/api/v1/login");
 
         public Predicate<ServerHttpRequest> isSecure = request -> openApiEndpoints.stream()
                         .noneMatch(uri -> request.getURI().getPath().contains(uri));
