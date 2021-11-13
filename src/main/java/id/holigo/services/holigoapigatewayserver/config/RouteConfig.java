c82be7e25cf526244d2bcd3d938078b68097a16d
dhaniemubarak <dhaniemubarak@gmail.com>
2021-11-13 08:23:54 +0700

    HOL-69 Modified Route Config

--- src/main/java/id/holigo/services/holigoapigatewayserver/config/RouteConfig.java
+++ src/main/java/id/holigo/services/holigoapigatewayserver/config/RouteConfig.java
@@ -3,7 +3,7 @@ package id.holigo.services.holigoapigatewayserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import id.holigo.services.holigoapigatewayserver.filters.AuthenticationFilter;
+import id.holigo.services.holigoapigatewayserver.web.filters.AuthorizationFilter;
 
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +11,25 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 
 @Configuration
 public class RouteConfig {
+        @Autowired
+        AuthorizationFilter authorizationFilter;
 
-    // @Autowired
-    // AuthenticationFilter authenticationFilter;
-
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("holigo-user-service",
-                        r -> r.path("/api/v1/user*", "/api/v1/user/**", "/api/v1/users*", "/api/v1/users/**")
-                                // .filters(f -> f.filter(authenticationFilter))
-                                .uri("lb://holigo-user-service"))
-                .route("holigo-otp-service", r -> r.path("/api/v1/otp/registration")
-                        // .filters()
-                        .uri("lb://holigo-otp-service"))
-                .build();
-    }
+        @Bean
+        public RouteLocator routes(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("holigo-oauth-service-login",
+                                                r -> r.path("/api/v1/login").uri("lb://holigo-oauth-service"))
+                                .route("holigo-oauth-service-test",
+                                                r -> r.path("/api/v1/test").filters(f -> f.filter(authorizationFilter))
+                                                                .uri("lb://holigo-oauth-service"))
+                                .route("holigo-user-service",
+                                                r -> r.path("/api/v1/user*", "/api/v1/user/**", "/api/v1/users*",
+                                                                "/api/v1/users/**")
+                                                                // .filters(f -> f.filter(authenticationFilter))
+                                                                .uri("lb://holigo-user-service"))
+                                .route("holigo-otp-service", r -> r.path("/api/v1/otp/registration")
+                                                // .filters()
+                                                .uri("lb://holigo-otp-service"))
+                                .build();
+        }
 }
