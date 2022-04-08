c705fc18cc566e6b153b65650ecac2084d6b9bee
dhaniemubarak <dhaniemubarak@gmail.com>
2022-04-08 14:06:53 +0700

    Fixed coupon route

--- src/main/java/id/holigo/services/holigoapigatewayserver/config/RouteConfig.java
+++ src/main/java/id/holigo/services/holigoapigatewayserver/config/RouteConfig.java
@@ -167,7 +167,7 @@ public class RouteConfig {
                                                                 .uri("lb://holigo-holiclub-service"))
                                 .route("holigo-coupon-service",
                                                 r -> r.path("/api/v1/coupons", "/api/v1/coupons**",
-                                                                "api/v1/coupons/**")
+                                                                "/api/v1/coupons/**")
                                                                 .filters(f -> f.filter(authorizationFilter))
                                                                 .uri("lb://holigo-coupon-service"))
                                 .build();
